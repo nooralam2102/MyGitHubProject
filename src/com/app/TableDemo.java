189ca4f81e72f976c850f5bd8ee0a987d8c5ae80
NOOR <NOOR@NOOR-PC>
2018-05-08 16:35:08 +0530

    First File change

---
+++ src/com/app/TableDemo.java
@@ -0,0 +1,16 @@
+package com.app;
+import java.util.Scanner;
+
+public class TableDemo {
+public static void main(String[] args) {
+	int n,i;
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter ur digit number");
+	n=Integer.parseInt(s.nextLine());
+	for(i=1;i<=10;i++ )
+	{
+		System.out.println(n+"*"+i+"="+(n*i));
+	}
+	
+}
+}
